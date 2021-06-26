37ab925bc12ca6197e57fb0eb755250d5504abd3
Oleg <brezitskyyoleg@gmail.com>
2021-06-26 17:35:47 +0200

    JSON generator initialization

---
+++ json_generator.py
@@ -0,0 +1,12 @@
+from xml_generator import NUMBER_OF_ENTRIES
+from common import *
+
+NUMBER_OF_ENTRIES = 1
+
+
+def main(requested_items):
+    pass
+
+
+if __name__ == "__main__":
+    main(NUMBER_OF_ENTRIES)
