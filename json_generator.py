b39d3e0e9cb0f3d7e8264a2c68c79d7ca303ccba
Oleg <brezitskyyoleg@gmail.com>
2021-06-26 17:43:23 +0200

    Minor changes

--- json_generator.py
+++ json_generator.py
@@ -1,11 +1,18 @@
-from xml_generator import NUMBER_OF_ENTRIES
+from xml_generator import NUMBER_OF_ENTRIES, OUTPUT_DIR
 from common import *
+import os
 
 NUMBER_OF_ENTRIES = 1
+OUTPUT_DIR = "data/json"
+
+
+def generate_bacteria(id_):
+    pass
 
 
 def main(requested_items):
-    pass
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
 
 
 if __name__ == "__main__":
